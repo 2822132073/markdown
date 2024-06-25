a3e87ee019bf98a59b20b6d82fe6bf085a92d600
冯思琳 <2822132073@qq.com>
2024-06-25 22:15:42 +0800

    添加了处理无法渲染文件的处理脚本

--- docs/deleteNotRenderFile.sh
+++ docs/deleteNotRenderFile.sh
@@ -8,9 +8,9 @@ if [ ! -f "$FILES_TO_DELETE" ]; then
   echo "文件 $FILES_TO_DELETE 不存在。"
   exit 1
 fi
-
 # 读取文件列表并删除相应的文件
-while IFS= read -r file_path
+file_paths=($(cat "$FILES_TO_DELETE"))
+for file_path in "${file_paths[@]}";
 do
   # 检查文件是否存在，然后删除
   if [ -f "$file_path" ]; then
