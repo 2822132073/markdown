156df0577d0d18949fc9008c73887a69ba519df7
冯思琳 <2822132073@qq.com>
2024-06-26 22:46:18 +0800

    修改vitepress配置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,7 @@ import AutoSidebar from 'vite-plugin-vitepress-auto-sidebar';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base : "/markdown/",
+  // base : "/markdown/", //如果域名后面有地址，这里就需要填写，例如2822132073.github.io/markdown 这里就需要填markdown
   title: "Fsl's Markdown site",
   description: "我的markdown笔记网站",
   themeConfig: {
