ff612eb5824d58f157437937a93732db2eca8d5f
2822132073 <63001285+2822132073@users.noreply.github.com>
2024-06-28 17:20:42 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -14,7 +14,22 @@ export default defineConfig({
 
     sidebar: [
     ],
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
 
+    outline: {
+      label: '页面导航'
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium'
+      }
+    },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
